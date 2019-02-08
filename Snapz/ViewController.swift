7e4b77a9727656553b94a3b0090d4caa067afe61
Nathan Glynn <nathanglynn@gmail.com>
2019-02-08 13:46:01 -0800

    Initial Commit

---
+++ Snapz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snapz
+//
+//  Created by Nathan Glynn on 2/8/19.
+//  Copyright © 2019 Nathan Glynn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
